85237b2b3ec6a8d81daffb90fae7303518846009
suprt <41486991+suprt@users.noreply.github.com>
2025-01-04 20:41:21 +0500

    Add files via upload

--- docs.go
+++ docs.go
@@ -174,7 +174,7 @@ const docTemplate = `{
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
 	Host:             "localhost:8080",
-	BasePath:         "",
+	BasePath:         "/",
 	Schemes:          []string{},
 	Title:            "GeoCode API",
 	Description:      "GeoCode API Server",
